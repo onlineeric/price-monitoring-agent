cf02c3b282bee15ef9688b8954925db21ac14194
Eric Cheng <online.eric@gmail.com>
2026-01-07 23:24:32 +1300

    [task-5.0] completed

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -4,6 +4,15 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
